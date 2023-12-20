5260906ff4038bdc2e1c7299cae5993af4633b26
denisoster <denis.oster.dev@gmail.com>
2023-12-20 06:54:21 +0200

    Refactor for ActiveAdmin 3.x

--- vendor/assets/javascripts/active_admin_date_range_preset.js
+++ vendor/assets/javascripts/active_admin_date_range_preset.js
@@ -1,206 +1,155 @@
-$(function () {
-  // options:
-  //  gteq_input: jQuery-selecotr for input date_from
-  //  lteq_input: jQuery-selecotr for input date_to
-  //  hours_offset: Int number - hours +/- to correct time
-  $.fn.date_range_ext_preset = function(options) {
-    // settings
-    options = options || {};
-    let opts = $.extend({}, $.fn.date_range_ext_preset.defaults, options);
+function num_with_leading_zero(num, digitsCount = 2) {
+  let s = num + '';
+  while (s.length < digitsCount) {
+    s = '0' + s;
+  }
+  return s;
+}
 
-    // aditional functions
-    function num_with_leading_zero(num, digitsCount = 2) {
-      let s = num + '';
-      while (s.length < digitsCount) {
-        s = '0' + s;
-      }
-      return s;
-    }
+function formatDate(date) {
+  let str = date.getFullYear() + '-' + num_with_leading_zero(date.getMonth() + 1) + '-' + num_with_leading_zero(date.getDate());
+  if (opts.show_time) {
+    str += (' ' + num_with_leading_zero(date.getHours()) + ':' + num_with_leading_zero(date.getMinutes()) + ':' + num_with_leading_zero(date.getSeconds()));
+  }
+  return str;
+}
 
-    // formated date YYYY-MM-DD, with converting to UTC
-    // note: getMonth Returns the month (from 0-11), so we do +1
-    function formatDate(date) {
-      let str = date.getFullYear() + '-' + num_with_leading_zero(date.getMonth()+1) + '-' + num_with_leading_zero(date.getDate());
-      if (opts.show_time) {
-        str += (' ' + num_with_leading_zero(date.getHours()) + ':' + num_with_leading_zero(date.getMinutes()) + ':' + num_with_leading_zero(date.getSeconds()));
-      }
-      return str;
-    }
+function unbindClickEventBlockTimerange() {
+  document.querySelectorAll('.block_timerange').forEach(function (el) {
+    el.remove();
+  });
+  document.body.removeEventListener('click', CalendarRangeSet);
+}
 
-    function unbindClickEventBlockTimerange() {
-      $('.block_timerange').remove();
-      $('body').off('click.CalendarRangeSet');
-    }
+let opts = {
+  hours_offset: 0,
+  date_to_human_readable: false,
+  show_time: false,
+  add_range: []
+};
 
-    // local datetime now
-    let now = new Date();
-    // UTC datetime now
-    let now_utc = new Date(
-      now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),
-      now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds(),
-      now.getUTCMilliseconds()
-    );
-    // datetime object, UTC with hours offset
-    let datetime = new Date(now_utc.getTime() + opts.hours_offset * 60 * 60 * 1000);
+document.addEventListener('DOMContentLoaded', function () {
 
-    // PLUGIN BEGIN
-    return this.each(function(i, el) {
-      let $this = $(el);
 
-      if (typeof $this.data('show-time') != 'undefined' && $this.data('show-time').toString() == 'true') {
-        opts.show_time = true;
-      }
+  let pairs = document.querySelectorAll('.datetime_preset_pair');
+  pairs.forEach(function (pair) {
+    init(pair)
+  });
 
-      // detect inputs
-      let lteq_input = $this.find('input:last');
-      if (options.lteq_input) {
-        lteq_input = $(options.lteq_input);
-      } else if ($this.hasClass('datetime_preset_pair')) {
-        lteq_input = $this.next().find('input');
-      }
+  document.querySelectorAll('form.filter_form div.filter_date_time_range').forEach(function (element) {
+    init(element)
+  });
 
-      let gteq_input = $this.find('input:first');
-      if (options.gteq_input) {
-        gteq_input = $(options.gteq_input);
-      } else if ($this.hasClass('datetime_preset_pair')) {
-        gteq_input = $this.find('input');
-      }
+});
 
-      // filter modifying
-      let main_btn_html = '<a href="#" class="btn_timerange">Set range</a>';
-      $this.find('label').addClass('datetime_preset_filter_label').append(main_btn_html);
+function CalendarRangeSet(e) {
+  e.stopPropagation();
+  if (!e.target.closest('.block_timerange')) {
+    unbindClickEventBlockTimerange();
+  }
+}
 
-      // helper
-      function fillInputs(start, end) {
-        gteq_input.val(formatDate(start));
-        if (opts.date_to_human_readable) {
-          end.setTime(end.getTime() - 1000);
-        }
+function fillInputs(start, end, gteqInput, lteqInput) {
+  gteqInput.value = formatDate(start);
+  if (opts.date_to_human_readable) {
+    end.setTime(end.getTime() - 1000);
+  }
+  lteqInput.value = formatDate(end);
+}
 
-        lteq_input.val(formatDate(end));
-      }
+function init(pair) {
+  let datetime = new Date();
+  let mainBtnHtml = '<a href="#" class="btn_timerange">Set range</a>';
+  let label = pair.querySelector('label');
+  label.classList.add('datetime_preset_filter_label');
+  label.insertAdjacentHTML('afterend', mainBtnHtml);
 
-      $this.on('click', '.btn_timerange', function(e) {
+  let btnTimeRange = pair.querySelector('.btn_timerange');
+  btnTimeRange.addEventListener('click', function (e) {
+    unbindClickEventBlockTimerange();
+    e.stopPropagation();
+    e.preventDefault();
+
+    let rangeElement = e.target.parentElement.querySelectorAll('input');
+    let gteqInput = rangeElement[0]; // From
+    let lteqInput = rangeElement[1]; // To
+
+    let additionalItemsHtml = '';
+    opts.add_range.forEach(function (el, i) {
+      additionalItemsHtml += '<div><span class="btn_date_range_' + i + '">' + el['title'] + '</span></div>';
+    });
+
+    let blockTimeRange = document.createElement('div');
+    blockTimeRange.style.minWidth = e.target.offsetWidth + 'px';
+    blockTimeRange.style.top = e.pageY - e.target.offsetTop + 'px';
+    blockTimeRange.style.left = e.pageX - e.target.offsetLeft + 'px';
+    blockTimeRange.classList.add('block_timerange');
+    blockTimeRange.innerHTML = `
+        <div><span class="btn_today">Today</span></div>
+        <div><span class="btn_yesterday">Yesterday</span></div>
+        <div><span class="btn_week">This Week</span></div>
+        <div><span class="btn_month">This Month</span></div>
+        <div><span class="btn_last_week">Last Week</span></div>
+        <div><span class="btn_last_month">Last Month</span></div>
+        ${additionalItemsHtml}
+      `;
+
+    document.body.appendChild(blockTimeRange);
+
+    let container = blockTimeRange;
+
+    opts.add_range.forEach(function (el, i) {
+      container.querySelector('.btn_date_range_' + i).addEventListener('click', function (e) {
         unbindClickEventBlockTimerange();
-        e.stopPropagation();
-        e.preventDefault();
-
-        let additional_items_html = '';
-        opts.add_range.forEach(function(el, i) {
-          return additional_items_html += '<div><span class="btn_date_range_' + i + '">' + el['title'] + '</span></div>';
-        });
-
-        $('body').append('<div style="min-width: '+e.target.offsetWidth+'px; top: '+(e.target.offsetTop)+'px; left: '+(e.target.offsetLeft)+'px" class="block_timerange">' +
-          '<div><span class="btn_today">Today</span></div>' +
-          '<div><span class="btn_yesterday">Yesterday</span></div>' +
-          '<div><span class="btn_week">This Week</span></div>' +
-          '<div><span class="btn_month">This Month</span></div>' +
-          '<div><span class="btn_last_week">Last Week</span></div>' +
-          '<div><span class="btn_last_month">Last Month</span></div>' +
-          additional_items_html +
-          '</div>'
-        ).ready(function() {
-          let container = $(this).find('.block_timerange');
-
-          // additional ranges
-          opts.add_range.forEach(function(el, i) {
-            $(container).on('click.CalendarRangeSet', '.btn_date_range_' + i, function(e) {
-              unbindClickEventBlockTimerange();
-              let start = new Date(el['start'].getFullYear(), el['start'].getMonth(), el['start'].getDate());
-              let end = new Date(el['end'].getFullYear(), el['end'].getMonth(), el['end'].getDate());
-              fillInputs(start, end)
-            });
-          });
-
-          // Today
-          $(container).on('click.CalendarRangeSet', '.btn_today', function(e) {
-            unbindClickEventBlockTimerange();
-            let start = new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate());
-            let end = new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate() + 1);
-            fillInputs(start, end);
-          });
-
-          // Yesterday
-          $(container).on('click.CalendarRangeSet', '.btn_yesterday', function(e) {
-            unbindClickEventBlockTimerange();
-            let start = new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate() - 1);
-            let end = new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate());
-            fillInputs(start, end);
-          });
-
-          // Week
-          $(container).on('click.CalendarRangeSet', '.btn_week', function(e) {
-            unbindClickEventBlockTimerange();
-            let start = new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate() - datetime.getDay() + 1);
-            let end = new Date(start.getFullYear(), start.getMonth(), start.getDate() + 7);
-            fillInputs(start, end);
-          });
-
-          // Month
-          $(container).on('click.CalendarRangeSet', '.btn_month', function(e) {
-            unbindClickEventBlockTimerange();
-            let start = new Date(datetime.getFullYear(), datetime.getMonth(), 1);
-            let end = new Date(datetime.getFullYear(), datetime.getMonth() + 1, 1);
-            fillInputs(start, end);
-          });
-
-          // Last Week
-          $(container).on('click.CalendarRangeSet', '.btn_last_week', function(e) {
-            unbindClickEventBlockTimerange();
-            let end = new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate() - datetime.getDay() + 1);
-            let start = new Date(end.getFullYear(), end.getMonth(), end.getDate() - 7);
-            fillInputs(start, end);
-          });
-
-          // Last Month
-          $(container).on('click.CalendarRangeSet', '.btn_last_month', function(e) {
-            unbindClickEventBlockTimerange();
-            let end = new Date(datetime.getFullYear(), datetime.getMonth(), 1);
-            let start = new Date(end.getFullYear(), end.getMonth() - 1, 1);
-            fillInputs(start, end);
-          });
-
-          // Outer
-          $('body').on('click.CalendarRangeSet', function(e) {
-            e.stopPropagation();
-            if ($(e.target).closest('.block_timerange').length == 0) {
-              unbindClickEventBlockTimerange();
-            }
-          });
-        });
+        let start = new Date(el['start'].getFullYear(), el['start'].getMonth(), el['start'].getDate());
+        let end = new Date(el['end'].getFullYear(), el['end'].getMonth(), el['end'].getDate());
+        fillInputs(start, end, gteqInput, lteqInput);
       });
     });
-  }
 
-  $.fn.date_range_ext_preset.defaults = {
-    // Manual global time shift, from UTC can be +/- number
-    hours_offset: 0,
+    container.querySelector('.btn_today').addEventListener('click', function (e) {
+      unbindClickEventBlockTimerange();
+      let start = new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate());
+      let end = new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate() + 1);
+      fillInputs(start, end, gteqInput, lteqInput);
+    });
 
-    // date_to_human_readable = true, then "date_to" consider as including full day without last second
-    //  For example Today will be:
-    //   true
-    //     2015-06-10 - 2015-06-10
-    //     2015-06-10 00:00:00 - 2015-06-10 23:59:59
-    //   false
-    //     2015-06-10 - 2015-06-11
-    //     2015-06-10 00:00:00 - 2015-06-11 00:00:00
-    date_to_human_readable: false,
+    container.querySelector('.btn_yesterday').addEventListener('click', function (e) {
+      unbindClickEventBlockTimerange();
+      let start = new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate() - 1);
+      let end = new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate());
+      fillInputs(start, end, gteqInput, lteqInput);
+    });
 
-    // Display time or not: 2015-06-10 vs 2015-06-10 00:00:00
-    show_time: false,
+    container.querySelector('.btn_week').addEventListener('click', function (e) {
+      unbindClickEventBlockTimerange();
+      let start = new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate() - datetime.getDay() + 1);
+      let end = new Date(start.getFullYear(), start.getMonth(), start.getDate() + 7);
+      fillInputs(start, end, gteqInput, lteqInput);
+    });
 
-    // Array of addition ranges
-    // example:
-    // {
-    //   title: 'Last 30 days',
-    //   start: new Date().setDate((new Date()).getDate() - 30)
-    //   end: new Date()
-    // }
-    add_range: []
-  }
-});
+    container.querySelector('.btn_month').addEventListener('click', function (e) {
+      unbindClickEventBlockTimerange();
+      let start = new Date(datetime.getFullYear(), datetime.getMonth(), 1);
+      let end = new Date(datetime.getFullYear(), datetime.getMonth() + 1, 1);
+      fillInputs(start, end, gteqInput, lteqInput);
+    });
 
-$(document).on('ready', function() {
-  // Init in forms
-  $('.datetime_preset_pair').date_range_ext_preset();
-});
+    container.querySelector('.btn_last_week').addEventListener('click', function (e) {
+      unbindClickEventBlockTimerange();
+      let end = new Date(datetime.getFullYear(), datetime.getMonth(), datetime.getDate() - datetime.getDay() + 1);
+      let start = new Date(end.getFullYear(), end.getMonth(), end.getDate() - 7);
+      fillInputs(start, end, gteqInput, lteqInput);
+    });
+
+    container.querySelector('.btn_last_month').addEventListener('click', function (e) {
+      unbindClickEventBlockTimerange();
+      let end = new Date(datetime.getFullYear(), datetime.getMonth(), 1);
+      let start = new Date(end.getFullYear(), end.getMonth() - 1, 1);
+      fillInputs(start, end, gteqInput, lteqInput);
+    });
+
+    document.body.addEventListener('click', CalendarRangeSet);
+
+  });
+}
